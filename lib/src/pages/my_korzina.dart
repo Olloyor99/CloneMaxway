5558d15411da154b4cf251430d182fa8d482003f
Olloyor <olloyornodirov1999@gmail.com>
2022-11-17 10:17:54 +0500

    jio

--- lib/src/pages/my_korzina.dart
+++ lib/src/pages/my_korzina.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:maxway_/list_name_price/l_photos.dart';
 import 'package:maxway_/list_name_price/narxlari.dart';
 import 'customhome.dart';
 
@@ -87,108 +88,119 @@ class MyKorzinaState extends State<MyKorzina> {
               child: ListView.builder(
                 itemCount: widget.forlist,
                 itemBuilder: ((context, index) => Container(
-                      height: 120,
+                      height: 126,
                       width: 339,
                       margin:
                           EdgeInsets.only(top: 2, bottom: 2, left: 3, right: 3),
                       decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 242, 239, 239),
+                          color: Colors.white,
                           borderRadius: BorderRadius.circular(12)),
-                      child: Row(
+                      child: Column(
                         children: [
-                          Container(
-                            height: 96,
-                            width: 96,
-                            alignment: Alignment.center,
-                            margin: EdgeInsets.only(left: 5),
-                            child: Text("Photo"),
-                          ),
-                          Column(
+                          Row(
                             children: [
-                              Row(
-                                children: [
-                                  Container(
-                                    width: 125,
-                                    height: 18,
-                                    margin: EdgeInsets.only(top: 12, left: 8),
-                                    child: Text(
-                                      "${lavashnomi[index].toString()}",
-                                      style: TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.w600),
-                                    ),
-                                  ),
-                                  Container(
-                                    width: 100,
-                                    height: 18,
-                                    alignment: Alignment.centerRight,
-                                    margin: EdgeInsets.only(
-                                      top: 12,
-                                      left: 12,
-                                    ),
-                                    child: Text(
-                                      "${lavashnarxi[index].toString()}, sum",
-                                      style: TextStyle(
-                                          color:
-                                              Color.fromARGB(255, 94, 40, 144),
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.w600),
-                                    ),
-                                  ),
-                                ],
+                              Container(
+                                height: 96,
+                                width: 96,
+                                alignment: Alignment.center,
+                                margin: EdgeInsets.only(left: 5),
+                                child: Image.asset(photos[index]),
                               ),
-                              SizedBox(
-                                height: 48,
-                              ),
-                              Row(
+                              Column(
                                 children: [
-                                  SizedBox(width: 12),
-                                  Container(
-                                    height: 32,
-                                    width: 32,
-                                    transformAlignment: Alignment.center,
-                                    decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius: BorderRadius.circular(8)),
-                                    child: IconButton(
-                                      onPressed: () {},
-                                      icon: Icon(
-                                        Icons.remove_outlined,
-                                        color: Colors.grey[700],
-                                        size: 17,
+                                  Row(
+                                    children: [
+                                      Container(
+                                        width: 125,
+                                        height: 18,
+                                        margin:
+                                            EdgeInsets.only(top: 12, left: 8),
+                                        child: Text(
+                                          "${lavashnomi[index].toString()}",
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w600),
+                                        ),
                                       ),
-                                    ),
-                                  ),
-                                  SizedBox(width: 12),
-                                  Text(
-                                    "1",
-                                    style: TextStyle(
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.w500),
-                                  ),
-                                  SizedBox(width: 12),
-                                  Container(
-                                    height: 32,
-                                    width: 32,
-                                    transformAlignment: Alignment.center,
-                                    decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius: BorderRadius.circular(8)),
-                                    child: IconButton(
-                                      onPressed: () {},
-                                      icon: Icon(
-                                        Icons.add,
-                                        color: Colors.grey[700],
-                                        size: 17,
+                                      Container(
+                                        width: 100,
+                                        height: 18,
+                                        alignment: Alignment.centerRight,
+                                        margin: EdgeInsets.only(
+                                          top: 12,
+                                          left: 12,
+                                        ),
+                                        child: Text(
+                                          "${lavashnarxi[index].toString()}, sum",
+                                          style: TextStyle(
+                                              color: Color.fromARGB(
+                                                  255, 94, 40, 144),
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w600),
+                                        ),
                                       ),
-                                    ),
+                                    ],
                                   ),
-                                  Container(
-                                    width: 150,
+                                  SizedBox(
+                                    height: 48,
+                                  ),
+                                  Row(
+                                    children: [
+                                      SizedBox(width: 12),
+                                      Container(
+                                        height: 32,
+                                        width: 32,
+                                        transformAlignment: Alignment.center,
+                                        decoration: BoxDecoration(
+                                            color: Colors.white,
+                                            borderRadius:
+                                                BorderRadius.circular(8)),
+                                        child: IconButton(
+                                          onPressed: () {},
+                                          icon: Icon(
+                                            Icons.remove_outlined,
+                                            color: Colors.grey[700],
+                                            size: 17,
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(width: 12),
+                                      Text(
+                                        "1",
+                                        style: TextStyle(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                      SizedBox(width: 12),
+                                      Container(
+                                        height: 32,
+                                        width: 32,
+                                        transformAlignment: Alignment.center,
+                                        decoration: BoxDecoration(
+                                            color: Colors.white,
+                                            borderRadius:
+                                                BorderRadius.circular(8)),
+                                        child: IconButton(
+                                          onPressed: () {},
+                                          icon: Icon(
+                                            Icons.add,
+                                            color: Colors.grey[700],
+                                            size: 17,
+                                          ),
+                                        ),
+                                      ),
+                                      Container(
+                                        width: 150,
+                                      )
+                                    ],
                                   )
                                 ],
-                              )
+                              ),
                             ],
+                          ),
+                          Divider(
+                            color: Colors.black,
+                            thickness: .1,
                           ),
                         ],
                       ),
@@ -196,50 +208,59 @@ class MyKorzinaState extends State<MyKorzina> {
               ),
             ),
             Container(
-              height: 56,
+              height: 60,
               margin: EdgeInsets.only(left: 12, right: 12),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
-                    bottomRight: Radius.circular(12),
-                    bottomLeft: Radius.circular(12)),
-              ),
-              child: Container(
-                margin: EdgeInsets.only(top: 3, left: 2, right: 2, bottom: 2),
-                decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 242, 239, 239),
-                    borderRadius: BorderRadius.circular(12)),
-                child: Row(
-                  children: [
-                    SizedBox(
-                      width: 12,
-                    ),
-                    Container(
-                      child: Text(
-                        "Сумма заказа",
-                        style: TextStyle(
-                          fontSize: 17,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 65,
-                    ),
-                    Container(
-                      width: 150,
-                      alignment: Alignment.centerRight,
-                      margin: EdgeInsets.only(right: 12),
-                      child: Text(
-                        "${widget.korzina2} cум",
-                        style: TextStyle(
-                          fontSize: 17,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    )
-                  ],
+                  bottomRight: Radius.circular(12),
+                  bottomLeft: Radius.circular(12),
                 ),
+              ),
+              child: Column(
+                children: [
+                  Divider(
+                    color: Colors.black,
+                    thickness: .1,
+                  ),
+                  Container(
+                    margin:
+                        EdgeInsets.only(top: 7, left: 2, right: 2, bottom: 2),
+                    decoration:
+                        BoxDecoration(borderRadius: BorderRadius.circular(12)),
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 12,
+                        ),
+                        Container(
+                          child: Text(
+                            "Сумма заказа",
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 65,
+                        ),
+                        Container(
+                          width: 150,
+                          alignment: Alignment.centerRight,
+                          margin: EdgeInsets.only(right: 12),
+                          child: Text(
+                            "${widget.korzina2} cум",
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
             SizedBox(height: 230),
